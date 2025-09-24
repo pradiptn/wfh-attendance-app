777b40906d89d29f17e1977bcb985ddf3cdfbd57
Nathaniel Pradipta <nathaniel.pradipta@gmail.com>
2025-09-24 08:47:32 +0000

    fix multer for photos managing

--- backend/src/attendance/attendance.module.ts
+++ backend/src/attendance/attendance.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
 import { Attendance } from '../entities/attendance.entity';
@@ -10,7 +12,13 @@ import { User } from '../entities/user.entity';
   imports: [
     TypeOrmModule.forFeature([Attendance, User]),
     MulterModule.register({
-      dest: './uploads',
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+          cb(null, `${randomName}${extname(file.originalname)}`);
+        },
+      }),
     }),
   ],
   providers: [AttendanceService],
