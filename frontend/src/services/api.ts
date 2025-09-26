1f00a5da33dfbcca56bec206deee26c290e91fd7
Nathaniel Pradipta <nathaniel.pradipta@gmail.com>
2025-09-26 08:58:37 +0000

    fix port issues

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000'; // API Gateway
+const API_BASE_URL = 'http://localhost:4000'; // API Gateway
 
 const api = axios.create({
   baseURL: API_BASE_URL,
